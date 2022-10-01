cd9b8eb438fc80bc4265d6def2801a01687cf871
TracyZhao2002 <tongyu0@ad.unc.edu>
2022-10-01 16:26:33 -0400

    a

---
+++ cli.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+import minimist from 'minimist';
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
+
+const args = minimist(process.argv.slice(2));
+const timezone = moment.tz.guest();
+
+if (args.h){
+  console.log(`
+  Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -t TIME_ZONE
+  -h            Show this help message and exit.
+  -n, -s        Latitude: N positive; S negative.
+  -e, -w        Longitude: E positive; W negative.
+  -t            Time zone: uses tz.guess() from moment-timezone by default.
+  -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+  -j            Echo pretty JSON from open-meteo API and exit.
+  `)
+}
+
+// declare latitude
+let latitude = '35.92'
+if (args.n) {
+    latitude = args.n
+}
+if (args.s) {
+    latitude = args.s
+}
+
+// declare longitude
+let longitude = '-79.05'
+if (args.e) {
+    longitude = args.e
+}
+if (args.w) {
+    longitude = args.w
+}
+
+// declare timezone
+if (args.t) {
+  timezone = args.t
+}
+
+// Make a request
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&current_weather=true&timezone=' + timezone);
+
+// Get the data from the request
+const data = await response.json();
+
+const days = args.d 
+
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
