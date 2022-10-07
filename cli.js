03120364a989ab2a860861c13753618e1f1c12d4
TracyZhao2002 <tongyu0@ad.unc.edu>
2022-10-06 22:07:57 -0400

    a

--- cli.js
+++ cli.js
@@ -6,16 +6,23 @@ import fetch from 'node-fetch';
 const args = minimist(process.argv.slice(2));
 let timezone = moment.tz.guess();
 
+
 if (args.h){
-  console.log(`
-  Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -t TIME_ZONE
-  -h            Show this help message and exit.
-  -n, -s        Latitude: N positive; S negative.
-  -e, -w        Longitude: E positive; W negative.
-  -t            Time zone: uses tz.guess() from moment-timezone by default.
-  -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-  -j            Echo pretty JSON from open-meteo API and exit.
+  try{
+    console.log(`
+      Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -t TIME_ZONE
+      -h            Show this help message and exit.
+      -n, -s        Latitude: N positive; S negative.
+      -e, -w        Longitude: E positive; W negative.
+      -t            Time zone: uses tz.guess() from moment-timezone by default.
+      -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+      -j            Echo pretty JSON from open-meteo API and exit.
   `)
+    process.exit(0);
+  }
+  catch(err){
+    process.exit(1);
+  }
 }
 
 // declare latitude
@@ -42,13 +49,13 @@ if (args.t) {
 }
 
 // Make a request
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&current_weather=true&timezone=' + timezone);
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&daily=precipitation_hours&current_weather=true&timezone=' + timezone);
 
 // Get the data from the request
 const data = await response.json();
 
 if(!args.h){
-  console.log(data)
+  console.log(data);
 
   const days = args.d 
 
